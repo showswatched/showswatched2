d88757119816c2b89a05a9a9b898384deb0c7f22
showswatched <ryanbertalotto@gmail.com>
2025-04-21 21:20:38 -0400

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/showswatched2/', // This line ensures correct asset paths for GitHub Pages
   plugins: [react()],
 })
